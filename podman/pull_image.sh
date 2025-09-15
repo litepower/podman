2636594e8171cef9e3cd6fa8798f80a1c90b11e8
yljos <yljos@outlook.com>
2025-09-15 15:47:51 +0800

    format

--- podman/pull_image.sh
+++ podman/pull_image.sh
@@ -2,11 +2,11 @@
 
 # 1. 仅更新正在运行的容器镜像
 podman ps --format "{{.Image}} {{.Names}}" | while read image name; do
-    echo "拉取镜像: $image"
-    podman image pull "$image"
-    echo "重启容器: $name"
-    clean_name="${name#systemd-}"
-    systemctl --user restart "$clean_name"
+	echo "拉取镜像: $image"
+	podman image pull "$image"
+	echo "重启容器: $name"
+	clean_name="${name#systemd-}"
+	systemctl --user restart "$clean_name"
 done
 
 # 2. 清理无用镜像
